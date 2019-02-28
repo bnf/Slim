03d9399a19450255adbf6d386b10fdacfc9f2f43
l0gicgate <pierre@lgse.com>
2019-02-28 14:00:55 -0700

    add Slim header comments to RouteResolverInterface

--- Slim/Interfaces/RouteResolverInterface.php
+++ Slim/Interfaces/RouteResolverInterface.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Slim Framework (https://slimframework.com)
+ *
+ * @link      https://github.com/slimphp/Slim
+ * @copyright Copyright (c) 2011-2018 Josh Lockhart
+ * @license   https://github.com/slimphp/Slim/blob/4.x/LICENSE.md (MIT License)
+ */
+
 declare(strict_types=1);
 
 namespace Slim\Interfaces;
